b118e0a1eeae5868bed3215e8f4a9e2d2998a21b
Lucas Oliveira <lucastxt@outlook.com>
2025-04-21 18:55:45 -0300

    feat: ajust input and add icon prop

--- src/views/components/Input/Input.vue
+++ src/views/components/Input/Input.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="input">
+  <div
+    :class="{
+      'input': true,
+      'input--error': hasErrors && !typing
+    }">
     <label
       :for="props.id"
-      :class="{
-        'input__label': true,
-        'input__label--error': hasErrors && !typing
-      }">
-        {{props.label}}
-      </label>
-    <input
-      :class="{
-        'input__field': true,
-        'input__field--error': hasErrors && !typing
-      }"
-      ref="input"
-      :required="props.required"
-      :id="props.id"
-      :name="props.name"
-      v-bind="$attrs"
-      v-mask="props.mask"
-      v-model="internalValue"
-      :placeholder="props.placeholder"
-      type="text"
-      @keydown.enter="pressEnter"
-      @input="handleInput"
-      @blur="validate"
-    />
-    <span v-if="hasErrors && !typing" class="input__error-message">
+      class="input__label">
+      {{props.label}}
+    </label>
+    <div class="input__wrapper">
+      <input
+        :class="{
+          'input__field': true,
+          'input__field--error': hasErrors && !typing
+        }"
+        ref="input"
+        :required="props.required"
+        :id="props.id"
+        :name="props.name"
+        v-bind="$attrs"
+        v-mask="props.mask"
+        v-model="internalValue"
+        :placeholder="props.placeholder"
+        :type="props.type"
+        @keydown.enter="pressEnter"
+        @input="handleInput"
+        @blur="validate"
+      />
+      <span
+        v-if="props.icon"
+        class="input__icon"
+        @click="clickIcon"
+      >
+        <i class="material-symbols-outlined">{{ props.icon }}</i>
+      </span>
+    </div>
+    <span v-show="hasErrors && !typing" class="input__error-message">
       <i class="material-symbols-outlined">error</i>
       {{ props.errorMessage }}
     </span>
@@ -40,6 +50,11 @@ const props = defineProps({
   modelValue: {
     type: [String, Number],
     default: '',
+  },
+  type: {
+    type: String,
+    default: 'text',
+    validator: (value) => ['text', 'email', 'password'].includes(value),
   },
   label: {
     type: String,
@@ -78,17 +93,21 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  icon: {
+    type: String,
+    default: '',
+  },
 });
 
 const internalValue = ref(props.modelValue || '');
 const hasErrors = ref(false);
 const typingTimeout = ref(null);
 const typing = ref(false);
-const emit = defineEmits(['update:modelValue', 'keydown-enter']);
+const emit = defineEmits(['update:modelValue', 'keydown-enter', 'click-icon']);
 
 watch(
   () => props.modelValue,
-  (newValue, oldValue)=> {
+  (newValue, oldValue) => {
     if (newValue !== oldValue) {
       internalValue.value = newValue;
     }
@@ -115,23 +134,33 @@ function handleInput(event) {
 function validate() {
   hasErrors.value = !props.hasValidated;
 }
+
+function clickIcon() {
+  emit('click-icon');
+}
 </script>
 
 <style lang="scss" scoped>
 .input {
   margin: 0;
-  padding-bottom: 12px;
+  padding-bottom: 18px;
   position: relative;
+  color: $color-text-primary;
+  &--error {
+    color: $color-ui-danger;
+  }
+
   &__label {
     font-size: $font-size-default;
     margin-left: 2px;
     margin-bottom: 6px;
     display: inline-block;
-    color: $color-text-primary;
+  }
 
-    &--error {
-      color: $color-ui-danger;
-    }
+  &__wrapper {
+    position: relative;
+    display: flex;
+    align-items: center;
   }
 
   &__field {
@@ -151,12 +180,24 @@ function validate() {
     }
   }
 
+  &__icon {
+    position: absolute;
+    right: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    i {
+      font-size: 20px;
+    }
+  }
+
   &__error-message {
     position: absolute;
     margin-top: 6px;
     color: $color-ui-danger;
     font-size: $font-size-mb-default;
-    font-weight: $font-weight-bold;
+    font-weight: $font-weight-semibold;
     display: flex;
     align-items: center;
     gap: 4px;
