b4b78fc0a32425dc9bbf59eca534386ff4a60601
Lucas Oliveira <lucastxt@outlook.com>
2025-04-21 18:57:49 -0300

    feat: adjust step three

--- src/views/modules/registration/components/StepThree.vue
+++ src/views/modules/registration/components/StepThree.vue
@@ -1,27 +1,86 @@
 <template>
-    <div>
-        <h2>Passo 3: Definir Senha</h2>
-        <form @submit.prevent="handleNext">
-            <Input v-model="formData.password" label="Senha" type="password" required />
-            <Button @click="previousStep">Voltar</Button>
-            <Button type="submit">Continuar</Button>
-        </form>
+    <div class="stap-three">
+        <Input
+            id="registration-field-password"
+            label="Senha"
+            :type="passwordType"
+            mask="E.repeat(50)"
+            placeholder="**********"
+            :icon="passwordIcon"
+            @clickIcon="handlerPasswordFieldType"
+            v-model="state.formData.password"
+            :errorMessage="stepThreeErros.password.message"
+            :hasValidated="stepThreeErros.password.valid"
+        />
+        <span class="stap-three__password-requires-label">A senha deve conter:</span>
+        <ul>
+            <li
+                v-for="req in passwordRequirements"
+                :key="req.key"
+                class="stap-three__requires-item"
+                :class="{
+                    'stap-three__requires-item': true,
+                    'stap-three__requires-item--met': req.met,
+                    'stap-three__requires-item--unmet': !req.met,
+                }">
+                <i class="stap-three__requires-icon material-symbols-outlined">
+                    {{ req.met ? 'task_alt' : 'error' }}
+                </i>
+                {{ req.label }}
+            </li>
+      </ul>
     </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue';
 import { useRegistrationStore } from '../store/registration-store';
 import Input from '@components/Input/Input.vue';
-import Button from '@components/Button/Button.vue';
 
-const { state, nextStep, previousStep } = useRegistrationStore();
-const formData = state.formData;
+const {
+    state,
+    stepThreeErros,
+    passwordRequirements
+} = useRegistrationStore();
 
-function handleNext() {
-    if (formData.password) {
-        nextStep();
-    } else {
-        alert('Por favor, preencha o campo de senha.');
+const showPassword = ref(false);
+
+const passwordIcon = computed(() => {
+  return showPassword.value ? "password" : "password_2";
+});
+const handlerPasswordFieldType = () => {
+  showPassword.value = !showPassword.value;
+};
+const passwordType = computed(() => {
+  return showPassword.value ? "text" : "password";
+});
+</script>
+
+<style lang="scss" scoped>
+.stap-three {
+    display: flex;
+    flex-direction: column;
+    &__password-requires-label {
+        margin: 16px 0 8px;
+        font-weight: $font-weight-semibold;
+    }
+
+    &__requires-icon {
+        font-size: $font-size-mb-default;
+    }
+    &__requires-item {
+        font-size: $font-size-mb-default;
+        font-weight: $font-weight-medium;
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        margin-bottom: 6px;
+        &--met {
+            color: $color-ui-success;
+        }
+        &--unmet {
+            color: $color-ui-danger;
+        }
     }
 }
-</script>
+</style>
